82bc8ef4ec8ad7fd08e9c39205386c580304cd1c
shane <shane.richards121@gmail.com>
2016-04-10 17:42:27 -0500

    removed unneeded files (forms, models, etc)

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask.ext.mysqldb import MySQL
+# from flask.ext.mysqldb import MySQL
 
-mysql = MySQL()
+# mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pdo_ret'
-app.config['MYSQL_HOST'] = 'localhost'
-mysql.init_app(app)
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'pdo_ret'
+# app.config['MYSQL_HOST'] = 'localhost'
+# mysql.init_app(app)
+
+app.config.from_object('config')
 
 from app import views
--- config/settings.py
+++
@@ -1,9 +0,0 @@
-class Config(Object):
-	DEBUG = False
-	TESTING = False
-	SECRET_KEY = "changeme"
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/comp3161'
-
-class DevConfig(Config):
-	TESTING = True
-	DEBUG = True
