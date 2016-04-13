9b35f5ae64bc2a23437ca25f56e52c097fab5569
shane <shane.richards121@gmail.com>
2016-04-13 01:40:44 -0500

    updated views

---
+++ app/static/js/register.js
@@ -0,0 +1,19 @@
+var working = false;
+$('.register').on('submit', function(e) {
+e.preventDefault();
+if (working) return;
+working = true;
+var $this = $(this),
+$state = $this.find('button > .state');
+$this.addClass('loading');
+$state.html('Registering');
+setTimeout(function() {
+$this.addClass('ok');
+$state.html('Welcome to the Online Meal Planner and Recipe System!');
+setTimeout(function() {
+$state.html('Register');
+$this.removeClass('ok loading');
+working = false;
+}, 4000);
+}, 3000);
+});
--- app/views.py
+++ app/views.py
@@ -17,7 +17,7 @@ def about():
 	return render_template('about.html')
 
 @app.route('/register', methods=['GET'])
-def register():
+def register():	
 	return render_template('register.html')
 
 @app.route('/login', methods=['GET'])
@@ -40,8 +40,38 @@ def authenticate():
 #				Query Pages
 # ====================================
 @app.route('/users', methods=['GET'])
-def all_users():
-	return 
+def user_all():
+	users = helpers.retrieve_all_users()
+	return render_template('users_all')
+
+@app.route('/users/<user_id>', methods=['GET'])
+def user_show_by_id(user_id):
+	users = helpers.retrieve_all_users()
+	return render_template('users_all')
+
+@app.route('/ingredients', methods=['GET'])
+def ingredients_all():
+	ingredients = helpers.retrieve_all_ingredients()
+	return render_template('ingredients_all')
+
+@app.route('/ingredients/<ingredient_id>', methods=['GET'])
+def ingredient_show_by_id(ingredient_id):
+	ingredients = helpers.retrieve_all_ingredients()
+	return render_template('ingredient_show')
+
+@app.route('/recipes', methods=['GET'])
+def recipes_all():
+	recipes = helpers.retrieve_all_recipes()
+	return render_template('recipes_all')
+
+@app.route('/recipes/<recipe_name>', methods=['GET'])
+def recipe_show_by_name(recipe_name):
+	recipe = helpers.retrieve_all_recipe()
+	return render_template('recipe_show')
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return 'This page does not exist', 404
 
 # @app.route('/Register',methods=['GET,POST'])
 # def register():
